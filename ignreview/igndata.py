9645661ab3d8db360375a66a6ef2695fe0489243
Matthew Hapenney <mattdh94@bu.edu>
2016-11-16 18:39:57 -0800

    added platform breakdown pie charts

--- ignreview/igndata.py
+++ ignreview/igndata.py
@@ -9,6 +9,7 @@ import scipy as spy
 import seaborn as sns
 sns.set_style("whitegrid")
 sns.set_context("notebook")
+sns.set_palette('Set2', 10)
 
 #%matplotlib inline
 print('Python_version: ' + sys.version)
@@ -31,23 +32,22 @@ print(sorted.head(1))
 df['release_year'].max()
 df['release_year'].min()
 
-df['score'].plot()
 #Only 1 game released in 1970, outlier
+df = df[df.release_year > 1970]
 
-df.score < 5 	#outputs false and trues for each
+#df.score < 5 	#outputs false and trues for each
 						#This series is called a mask
 						#Count number of trues and divide by total, get fraction of ratings <5
-np.sum(df.score < 5)
-np.sum(df.score < 5)/float(df.shape[0] )	#cant do in python 2.x because division is integer divison by default - must convert to float
-np.mean(df.score < 5)	#exact same as (np.sum(df.score < 5)/float(df.shape[0] ))
-(df.score < 5).mean()	#exact same just in Pandas
+#np.sum(df.score < 5)
+#np.sum(df.score < 5)/float(df.shape[0] )	#cant do in python 2.x because division is integer divison by default - must convert to float
+#np.mean(df.score < 5)	#exact same as (np.sum(df.score < 5)/float(df.shape[0] ))
+#(df.score < 5).mean()	#exact same just in Pandas
 
-#filtering datafram
-(df.query("score > 5.0"))
-(df[df.release_year < 2012])		#create mask and use to 'index' into dataframe to get rows we want
-(df[(df.release_year < 2012) & (df.score > 5.0)]) #combine these 2 conditions, uses boolean AND
+#filtering dataframe
+#(df.query("score > 5.0"))
+#(df[df.release_year < 2012])		#create mask and use to 'index' into dataframe to get rows we want
+#(df[(df.release_year < 2012) & (df.score > 5.0)]) #combine these 2 conditions, uses boolean AND
 
-#insert for loop for automation
 #filter dataframe by year
 '''
 checker = np.logical_and((df.release_year > 1999), (df.score > 5.0))
@@ -103,7 +103,7 @@ plt.ylabel('Counts')
 plt.title('Game Score Histogram')
 plt.legend()
 					#low review numbers are very sparse in comparison to closer to average
-					#Might have to regularize our model for recommendation
+					#Might have to regularize  model for recommendation
 #setting the alpha transparency low we can show how density of highly rated games has changed over time
 
 with sns.axes_style('darkgrid'):
@@ -116,12 +116,51 @@ with sns.axes_style('darkgrid'):
 	plt.title('Game Score over Time')
 	#utilized series in x-list and y-list slots in scatter function of plt mod
 
-#python is a duck
-#Pandas series quacks like a python list so plt.scatter will accept it
-
 #Operations on numpy arrays and by extension, pd series, are vectorized
-#Can add numpy arrays by using '+'
 
+
+
+#apply parser(lambda) to date/year of release to condense
+release_info = df.apply(lambda x: pd.datetime.strptime('{0} {1} {2} 00:00:00'.format(x['release_year'], x['release_month'], x['release_day']), "%Y %m %d %H:%M:%S"), axis = 1)
+df['release'] = release_info
+
+print(df['release'].head())
+
+month_list = ['January','February','March','April','May','June','July','August',
+				'September','October','November','December']
+
+with sns.axes_style('darkgrid'):
+
+	plt.figure(figsize = (15,8))
+	df.groupby(['release_year']).size().plot(kind = 'bar')
+	plt.xlabel('Release Year')
+	plt.ylabel('Game Counts')
+	plt.title('Games Released by Year')
+
+	plt.figure(figsize = (10,6))
+	df.groupby(['release_month']).size().plot(c = 'b')
+	plt.xlabel('Release Month')
+	plt.ylabel('Game Counts')
+	plt.title('Games Released by Month(1996-2016')
+	plt.xticks(range(1,13), month_list)
+
+	plt.figure(figsize = (10,6))
+	df.groupby(['release_day']).size().plot(c = 'g')
+	plt.xlabel('Release Day')
+	plt.ylabel('Game Counts')
+	plt.title('Games Released by Date of Month (1996-2016)')
+
+	plt.figure(figsize = (10,6))
+	df.groupby(['release']).size().plot()
+	plt.title('Release per days (entire datset)')
+
+with sns.axes_style('whitegrid'):
+	fig, ax = plt.subplots()
+	plt.hist(df.release_year.values, bins = 100, alpha = 0.5)
+	plt.xlim([1995, 2017])
+	plt.xlabel('Year')
+	plt.ylabel('Game Counts')
+	plt.title('Games Reviewed by IGN each year')
 
 genre_count = df['genre'].value_counts()
 score_count = df['score_phrase'].value_counts()
@@ -131,7 +170,9 @@ print(genre_count.head(10))
 
 ordered_score = ['Disaster', 'Unbearable' ,'Painful' ,'Awful' ,'Bad', 'Mediocre', 
                  'Okay' ,'Good' ,'Great', 'Amazing', 'Masterpiece']
-list_genre = ['Action', 'Sports', 'Shooter', 'Racing', 'Adventure', 'Strategy', 'RPG', 'Platformer', 'Puzzle','Action, Adventure']
+
+list_genre = ['Action', 'Sports', 'Shooter', 'Racing', 'Adventure', 'Strategy', 
+				'RPG', 'Platformer', 'Puzzle','Action, Adventure']
 
 genre_counts = []
 for score in list_genre:
@@ -141,49 +182,8 @@ counts = []
 for score in ordered_score:
 	counts.append(score_count[score])
 
-#Filter by platform
-
-print(df.platform.unique())
-
-
-#apply parser(lambda) to date/year of release to condense
-release_info = df.apply(lambda x: pd.datetime.strptime('{0} {1} {2} 00:00:00'.format(x['release_year'], x['release_month'], x['release_day']), "%Y %m %d %H:%M:%S"), axis = 1)
-df['release_date'] = release_info
-
-print(df['release_date'].head())
-with sns.axes_style('darkgrid'):
-
-	plt.figure(figsize = (15,8))
-	df.groupby(['release_year']).size().plot(c = 'b')
-	plt.xlabel('Release Year')
-	plt.ylabel('Game Counts')
-	plt.title('Games Released by Year')
-
-	plt.figure(figsize = (10,6))
-	df.groupby(['release_month']).size().plot(c = 'r')
-	plt.xlabel('Release Month')
-	plt.ylabel('Game Counts')
-	plt.title('Games Released by Month(1996-2016')
-
-	plt.figure(figsize = (10,6))
-	df.groupby(['release_day']).size().plot(c = 'g')
-	plt.xlabel('Release Day')
-	plt.ylabel('Game Counts')
-	plt.title('Games Released by Day # (1996-2016)')
-
-	plt.figure(figsize = (10,6))
-	df.groupby(['release_date']).size().plot()
-	plt.xlabel('Y/M/D')
-	plt.ylabel('Game Counts')
-	plt.title('Release DATE')
-
-with sns.axes_style('whitegrid'):
-	fig, ax = plt.subplots()
-	plt.hist(df.release_year.values, bins = 100, alpha = 0.5)
-	plt.xlim([1995, 2017])
-	plt.xlabel('Year')
-	plt.ylabel('Game Counts')
-	plt.title('Games Reviewed by IGN each year')
+avg = df.groupby('score_phrase')['score'].mean().sort_values
+print(avg)
 
 
 fig, ax = plt.subplots(figsize = (11,8))
@@ -196,16 +196,57 @@ fig, ax = plt.subplots(figsize = (11,8))
 sns.barplot(x = list_genre, y = genre_counts, color = '#96f97b')
 ax.set(ylabel = 'Count', xlabel = 'Genre')
 sns.plt.title('Games by Genre')
-#ticks = plt.setp(ax.get_xticklabels(), rotation = 30, fontsize = 8)
+ticks = plt.setp(ax.get_xticklabels(), rotation = 30, fontsize = 8)
+
+#Filter by platform
+
+print(df.platform.unique())
+print(df.platform.value_counts())
+
+#Pie graph platform breakdown
+#First top 10 popular platforms
+
+#Platforms by year
+f, ax = plt.subplots(2,3, figsize = (10,10))
+year2001 = df[df.release_year == 2001]	#release of the xbox/ nintendo gamecube
+year2002 = df[df.release_year == 2002]
+year2005 = df[df.release_year == 2005]	#Release of the xbox360
+year2006 = df[df.release_year == 2006]	#Release of the ps3/wii
+year2013 = df[df.release_year == 2013]	#Release of the PS4 & XboxOne
+year2014 = df[df.release_year == 2014]
 
 
-#pie chart of games released per system
-#Separate charts per each year
+pop1 = year2001.platform.value_counts()[year2001.platform.value_counts() > 3]
+pop1.plot.pie(ax = ax[0,0])
+ax[0,0].set_title('2001')
+ax[0,0].set_ylabel('')
 
-#sums = df.platform.groupby(df.release_year).sum()
-#axis('equal');
+pop2 = year2002.platform.value_counts()[year2002.platform.value_counts() > 3]
+pop2.plot.pie(ax = ax[0,1])
+ax[0,1].set_title('2002')
+ax[0,1].set_ylabel('')
+
+pop3 = year2005.platform.value_counts()[year2005.platform.value_counts() > 3]
+pop3.plot.pie(ax = ax[0,2])
+ax[0,2].set_title('2005')
+ax[0,2].set_ylabel('')
+
+pop4 = year2006.platform.value_counts()[year2006.platform.value_counts() > 3]
+pop4.plot.pie(ax = ax[1,0])
+ax[1,0].set_title('2006')
+ax[1,0].set_ylabel('')
+
+pop5 = year2013.platform.value_counts()[year2013.platform.value_counts() > 3]
+pop5.plot.pie(ax = ax[1,1])
+ax[1,1].set_title('2013')
+ax[1,1].set_ylabel('')
+
+pop6 = year2014.platform.value_counts()[year2014.platform.value_counts() > 3]
+pop6.plot.pie(ax = ax[1,2])
+ax[1,2].set_title('2014')
+ax[1,2].set_ylabel('')
 
 
-"""
+
 plt.show()
 
