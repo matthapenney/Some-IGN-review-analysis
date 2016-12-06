f29510659b70e3639dcf246a5a68343006ac84af
Matthew Hapenney <mattdh94@bu.edu>
2016-12-06 13:30:29 -0800

    deleted year filter lines

--- ignreview/igndata.py
+++ ignreview/igndata.py
@@ -63,36 +63,19 @@ for year in df.release_year:
 		year = year + 1
 	elif checker2:
 		break
-
-df[(df.release_year == 2000) & (df.score >5.0)]	#year 2000
-df[(df.release_year == 2001) & (df.score >5.0)] #year 2001
-df[(df.release_year == 2002) & (df.score >5.0)]	#year 2002
-df[(df.release_year == 2003) & (df.score >5.0)] #year 2003
-df[(df.release_year == 2004) & (df.score >5.0)]	#year 2004
-df[(df.release_year == 2005) & (df.score >5.0)]	#year 2005
-df[(df.release_year == 2006) & (df.score >5.0)]	#year 2006
-df[(df.release_year == 2007) & (df.score >5.0)]	#year 2007
-df[(df.release_year == 2008) & (df.score >5.0)]	#year 2008
-df[(df.release_year == 2009) & (df.score >5.0)]	#year 2009
-df[(df.release_year == 2010) & (df.score >5.0)]	#year 2010
-df[(df.release_year == 2011) & (df.score >5.0)]	#year 2011
-df[(df.release_year == 2012) & (df.score >5.0)]	#year 2012
-df[(df.release_year == 2013) & (df.score >5.0)]	#year 2013
-df[(df.release_year == 2014) & (df.score >5.0)]	#year 2014
-df[(df.release_year == 2015) & (df.score >5.0)]	#year 2015
-df[(df.release_year == 2016) & (df.score >5.0)]	#year 2016
 """
 
 ##CLEANING
 df['editors_choice'] = df.editors_choice.astype(bool)
 
 print(df.isnull())			#check if we are missing any data
-							#output is mostly all false so mostly all data?
+							#output is mostly all false so mostly all data
 df.isnull().values.any()	#fastest way to check whether any value is missing
-df.isnull().sum()			#36 times genre is missing :( sad face
+missing = df.isnull().sum()			#36 times genre is missing :( sad face
+print('missing:', missing)
 
-df = df[df.genre.notnull()]
-df.isnull().sum()			#Removed incomplete data (missing genre)
+df = df[df.genre.notnull()]	#Removed rows incomplete data
+print(df.isnull().sum()	)		
 
 
 #Visualize
@@ -100,11 +83,11 @@ mean_score = np.mean(df.score)
 print(mean_score)
 
 df.score.hist(bins=50, alpha=0.5);	
-plt.axvline(mean_score, 0, 0.75, color = 'r', label = 'Mean')
-plt.xlabel('Average Score of Game')
-plt.ylabel('Counts')
-plt.title('Game Score Histogram')
-plt.legend()
+_ = plt.axvline(mean_score, 0, 0.75, color = 'r', label = 'Mean')
+_ = plt.xlabel('Average Score of Game')
+_ = plt.ylabel('Counts')
+_ = plt.title('Game Score Histogram')
+_ = plt.legend()
 
 
 #low review numbers are very sparse in comparison to closer to average
@@ -113,12 +96,12 @@ plt.legend()
 
 with sns.axes_style('darkgrid'):
 	fig, (ax1, ax2) = plt.subplots(2, sharex = True, sharey = True)
-	ax1.scatter(df.release_year, df.score,lw= 0, alpha = .05)
-	sns.kdeplot(df.release_year,df.score, ax = ax2, n_levels = 20, cmap = 'Blues',shade = True, shade_lowest = False)
-	plt.xlim([1995,2017])
-	plt.xlabel('Year')
-	plt.ylabel('Score(1-10)')
-	plt.title('Game Score over Time')
+	_ = ax1.scatter(df.release_year, df.score,lw= 0, alpha = .05)
+	_ = sns.kdeplot(df.release_year,df.score, ax = ax2, n_levels = 20, cmap = 'Blues',shade = True, shade_lowest = False)
+	_ = plt.xlim([1995,2017])
+	_ = plt.xlabel('Year')
+	_ = plt.ylabel('Score(1-10)')
+	_ = plt.title('Game Score over Time')
 	#utilized series in x-list and y-list slots in scatter function of plt mod
 
 #Operations on numpy arrays and by extension, pd series, are vectorized
@@ -136,39 +119,40 @@ month_list = ['January','February','March','April','May','June','July','August',
 
 with sns.axes_style('darkgrid'):
 
-	plt.figure(figsize = (15,8))
-	df.groupby(['release_year']).size().plot(kind = 'bar')
-	plt.xlabel('Release Year')
-	plt.ylabel('Game Counts')
-	plt.title('Games Released by Year')
+	_ = plt.figure(figsize = (15,8))
+	_ = df.groupby(['release_year']).size().plot(kind = 'bar')
+	_ = plt.xlabel('Release Year')
+	_ = plt.ylabel('Game Counts')
+	_ =plt.title('Games Released by Year')
 
-	plt.figure(figsize = (10,6))
-	df.groupby(['release_month']).size().plot(c = 'b')
-	plt.xlabel('Release Month')
-	plt.ylabel('Game Counts')
-	plt.title('Games Released by Month(1996-2016')
-	plt.xticks(range(1,13), month_list)
+	_ = plt.figure(figsize = (10,6))
+	_ = df.groupby(['release_month']).size().plot(c = 'b')
+	_ = plt.xlabel('Release Month')
+	_ = plt.ylabel('Game Counts')
+	_ = plt.title('Games Released by Month(1996-2016')
+	_ = plt.xticks(range(1,13), month_list)
 
-	plt.figure(figsize = (10,6))
-	df.groupby(['release_day']).size().plot(c = 'g')
-	plt.xlabel('Release Day')
-	plt.ylabel('Game Counts')
-	plt.title('Games Released by Date of Month (1996-2016)')
+	_ = plt.figure(figsize = (10,6))
+	_ = df.groupby(['release_day']).size().plot(c = 'g')
+	_ = plt.xlabel('Release Day')
+	_ = plt.ylabel('Game Counts')
+	_ = plt.title('Games Released by Date of Month (1996-2016)')
 
-	plt.figure(figsize = (10,6))
-	df.groupby(['release']).size().plot()
-	plt.title('Release per days (entire datset)')
+	_ = plt.figure(figsize = (10,6))
+	_ = df.groupby(['release']).size().plot()
+	_ = plt.title('Game release per days of month')
 
 with sns.axes_style('whitegrid'):
 	fig, ax = plt.subplots()
-	plt.hist(df.release_year.values, bins = 100, alpha = 0.5)
-	plt.xlim([1995, 2017])
-	plt.xlabel('Year')
-	plt.ylabel('Game Counts')
-	plt.title('Games Reviewed by IGN each year')
+	_ = plt.hist(df.release_year.values, bins = 100, alpha = 0.5)
+	_ = plt.xlim([1995, 2017])
+	_ = plt.xlabel('Year')
+	_ = plt.ylabel('Game Counts')
+	_ = plt.title('Games Reviewed by IGN each year')
 
 genre_count = df['genre'].value_counts()
 score_count = df['score_phrase'].value_counts()
+
 #year_numba = df['release_year'].value_counts()
 print(genre_count.head(10))
 
@@ -183,25 +167,25 @@ genre_counts = []
 for score in list_genre:
     genre_counts.append(genre_count[score])
 
-
 counts = []   
 for score in ordered_score:
 	counts.append(score_count[score])
 
+
 avg = df.groupby('score_phrase')['score'].mean().sort_values
-print(avg)
+print('Average:', avg)
 
 
 fig, ax = plt.subplots(figsize = (11,8))
-sns.barplot(x = ordered_score, y = counts, color = '#00FFFF')
-ax.set(ylabel = 'Count', xlabel = 'Score Words')
-sns.plt.title('Games by Score Description')
+_ = sns.barplot(x = ordered_score, y = counts, color = '#00FFFF')
+_ = ax.set(ylabel = 'Count', xlabel = 'Score Words')
+_ = sns.plt.title('Games by Score Description')
 ticks = plt.setp(ax.get_xticklabels(), rotation = 30, fontsize = 8)
 
 fig, ax = plt.subplots(figsize = (11,8))
-sns.barplot(x = list_genre, y = genre_counts, color = '#96f97b')
-ax.set(ylabel = 'Count', xlabel = 'Genre')
-sns.plt.title('Games by Genre')
+_  = sns.barplot(x = list_genre, y = genre_counts, color = '#96f97b')
+_  = ax.set(ylabel = 'Count', xlabel = 'Genre')
+_  = sns.plt.title('Games by Genre')
 ticks = plt.setp(ax.get_xticklabels(), rotation = 30, fontsize = 8)
 
 #Filter by platform
@@ -267,11 +251,11 @@ print("unicorn_y:", unicorn_y)
 
 #slope, intercept, r_value, p_value, std_err = stats.linregress(unicorn1,unicorn2)
 
-#sm.add_constant(unicorn_x) adds a column of 1s to unicorn_x to get intercept term
+#sm.add_constant(unicorn_x) =  adds a column of 1s to unicorn_x to get intercept term
 unicorn_x = sm.add_constant(unicorn_x)
 
 results = sm.OLS(unicorn_y, unicorn_x).fit()
-
+print(results)
 
 #Linear regression
 ''''
@@ -280,19 +264,22 @@ x_plot = np.linspace(0,1,100)
 plt.plot(X_plot, X_plot*results.params[0] + results.params[1])
 plt.title("Linear Regressson (Score v. Release_month")
 '''
+
 #Moving Average - Time Series (release date ) vs. score
 
 dates = pd.date_range(df.index.min(), df.index.max())
 reindexed = df.reindex(dates)
-print(reindexed.head())
+print(reindexed)
+
 roll_mean = pd.rolling_mean(reindexed.score, 30)
 
 ewma = pd.stats.moments.ewma
 ewma(reindexed.score, span = 30)
 
-plt.scatter(roll_mean.index, roll_mean)
-plt.title('Work in Progress')
+fig, ax = plt.subplots()
+_ = plt.scatter(x = roll_mean.index, y = roll_mean)
+_ = plt.title('Work in Progress')
 
 
-#plt.show()
+plt.show()
 
